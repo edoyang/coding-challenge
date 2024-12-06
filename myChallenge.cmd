bb0590909ca21f310f873f632c85527ba9e5f381
edoyang <edoyangz@gmail.com>
2024-12-07 09:57:16 +1100

    automation running the instances

---
+++ myChallenge.cmd
@@ -0,0 +1,2 @@
+@echo off
+node_modules\.bin\ts-node index.ts
--- rewardpay/src/pages/Dataset/index.jsx
+++ rewardpay/src/pages/Dataset/index.jsx
@@ -29,13 +29,13 @@ const Dataset = () => {
             }`}
             key={index}
             onClick={() => !openAll && handleClick(index)}>
+            <p>Account name: {item.account_name}</p>
             <p>Account identifier: {item.account_identifier}</p>
             <p>Account category: {item.account_category}</p>
             <p>Account code: {item.account_code}</p>
             <p>Account currency: {item.account_currency}</p>
             <p>Account status: {item.account_status}</p>
             <p>Value type: {item.value_type}</p>
-            <p>Account name: {item.account_name}</p>
             <p>Account type: {item.account_type}</p>
             <p>Account type bank: {item.account_type_bank}</p>
             <p>System account: {item.system_account}</p>
